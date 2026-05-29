45e1c5c98d241b3ef3e0478ce2f3c10c353ea838
Łukasz Pławiak <lukaszplawiakzero1@gmail.com>
2026-05-29 17:51:23 +0200

    chore: initial project setup from Spring Initializr

---
+++ README.md
@@ -0,0 +1,1 @@
+TODO at the end
---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.atipera"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-resttestclient")
+	testImplementation("org.springframework.boot:spring-boot-restclient")
+	testImplementation("org.wiremock.integrations:wiremock-spring-boot:4.2.1")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
